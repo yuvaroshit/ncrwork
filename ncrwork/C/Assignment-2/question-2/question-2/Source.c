3760615a85fbb6e840ea8bc66a7d974117cdc441
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-06 22:40:31 -0800

    q2

---
+++ ncrwork/C/Assignment-2/question-2/question-2/Source.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n, rem, dec = 0, i = 0;
+	scanf_s("%d", &n);
+	while (n != 0)
+	{
+		rem = n % 10;
+		n = n / 10;
+		dec = dec + rem * pow(2, i);
+		i++;
+
+	}
+
+	printf("%X", dec);
+	getch();
+	return 0;
+}
