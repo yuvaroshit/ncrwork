27631a6ac52e89c88a125f82696cbb8857dafcdc
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-12 00:51:45 -0800

    q2

---
+++ ncrwork/C/Assignment-5/question-2/question-2/Source.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct str
+{
+	int data;
+	char ch;
+
+}s;
+union un
+{
+	int data1;
+	char ch1;
+}u;
+int main()
+{
+	printf("Size of structure is %d\n", sizeof(s));
+	printf("Size of union is %d", sizeof(u));
+	getch();
+	return 0;
+
+}
