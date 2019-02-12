d6a9823ade88f27c2b5bc3b323fc5c50613a2feb
yuvaroshit <yuvaroshit@gmail.com>
2019-02-12 01:50:29 -0800

    q3

--- ncrwork/C/Assignment-5/question-3/question-3/Source.c
+++ ncrwork/C/Assignment-5/question-3/question-3/Source.c
@@ -1,6 +1,44 @@
 #include<stdio.h>
-enum operations { addition, subtraction };
+enum operations { add, sub };
+struct complex
+{
+	int real, img;
+}a, b, c;
+
+void complexadd()
+{
+	printf("enter numbers\n");
+	scanf("%d %d", &a.real, &a.img);
+	scanf("%d %d", &b.real, &b.img);
+	c.real = a.real + b.real;
+	c.img = a.img + b.img;
+	printf("%d + %di", c.real, c.img);
+
+}
+void complexsub()
+{
+	printf("enter numbers\n");
+	scanf("%d %d", &a.real, &a.img);
+	scanf("%d %d", &b.real, &b.img);
+	c.real = a.real - b.real;
+	c.img = a.img - b.img;
+	printf("%d %di", c.real, c.img);
+
+}
 int main()
 {
 
+	int x;
+	printf("enter 0 for add\n 1 for sub\n");
+	scanf("%d", &x);
+	switch (x)
+	{
+	case add:complexadd();
+		break;
+	case sub:complexsub();
+		break;
+	default:printf("invalid");
+	}
+	getch();
+	return 0;
 }
