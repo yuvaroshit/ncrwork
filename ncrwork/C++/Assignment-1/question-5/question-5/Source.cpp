3691eeb645c13bf397ffe5bf69849d1d3af3112e
yuvaroshit <yuvaroshit@gmail.com>
2019-02-14 01:20:17 -0800

    q5

---
+++ ncrwork/C++/Assignment-1/question-5/question-5/Source.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+class Distance1
+{
+public:
+	float mt;
+	int cm;
+public:
+	void getdata(void);
+	void display(void);
+	friend Distance1 add(Distance1, Distance1);
+};
+
+class Distance2
+{
+	int feet;
+	float inches;
+public:
+	void getdata(void);
+	void display(void);
+	friend Distance1 add(Distance1, Distance2);
+};
+
+void Distance1::getdata(void)
+{
+	cout << "\n\nEnter Values for metres :-";
+	cin >> mt;
+	cout << "Enter Values for centimetres:-";
+	cin >> cm;
+}
+void Distance1::display(void)
+{
+	cout << "\n\nThe value of distance in metres is " << mt;
+	cout << "\nThe value of distance in Centimetres is " << cm;
+}
+
+
+void Distance2::getdata(void)
+{
+	cout << "\n\nEnter Values for feet :-";
+	cin >> feet;
+	cout << "Enter Values for inches :-";
+	cin >> inches;
+}
+void Distance2::display(void)
+{
+	cout << "\n\nThe value of distance in feet is " << feet;
+	cout << "\nThe value of distance in inches is " << inches;
+}
+
+Distance1 add( Distance1 a, Distance2  b)
+{
+	Distance1 temp;
+	temp.cm = a.cm + (b.feet * 30) + ((b.inches * 30) / 12.0);
+	temp.mt = a.mt + (temp.cm % 100);
+	temp.cm = temp.cm - ((temp.cm % 100) * 100);
+	return(temp);
+}
+
+int main()
+{
+	Distance1 a;
+	a.getdata();
+	Distance2 b;
+	b.getdata();
+
+	cout << "\n\t\tAFTER CONVERSION AND THEIR ADDITION IS PERFORMED\n";
+
+	Distance1 extra;
+	extra = add(a, b);
+	extra.display();
+
+	return 0;
+	getchar();
+}
