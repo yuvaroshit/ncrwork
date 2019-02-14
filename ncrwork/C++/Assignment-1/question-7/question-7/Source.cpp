d70d152f6300357df724a35d930b1c1a23fdbc4f
yuvaroshit <yuvaroshit@gmail.com>
2019-02-14 02:53:03 -0800

    q7

--- ncrwork/C++/Assignment-1/question-7/question-7/Source.cpp
+++ ncrwork/C++/Assignment-1/question-7/question-7/Source.cpp
@@ -1,1 +1,40 @@
 #include<iostream>
+using namespace std;
+class mat
+{
+	int a[3][3] = { { 1,2,3 },{ 2,3,4 },{ 3,4,5 } };
+	int b[3][1] = { { 1 },{ 2 },{ 3 } };
+	int c[3][3];
+public:
+	friend void mul(mat m1);
+};
+void mul(mat m1)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			int sum = 0;
+			for (int k = 0; k < 3; k++)
+			{
+				sum = sum + m1.a[i][k] * m1.a[k][j];
+			}
+			m1.c[i][j] = sum;
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << m1.c[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	mat m1;
+	mul(m1);
+	getchar();
+	return 0;
+}
