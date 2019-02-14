bcd14784e795c6a857b31a0ad9fc03124fd68978
yuvaroshit <yuvaroshit@gmail.com>
2019-02-14 02:32:28 -0800

    q8

---
+++ ncrwork/C++/Assignment-1/question-8/question-8/Source.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Employee
+{
+	char name[30];
+	int id;
+	float sal;
+public:
+	Employee()
+	{
+		id = 0;
+		sal = 0;
+		cout << "Default Constructor is invoked" << endl;
+
+	}
+	Employee(char nm[30], int id1, float sal1)
+	{
+		strcpy_s(name, nm);
+		id = id1;
+		sal = sal1;
+	}
+	~Employee()
+	{
+		cout << "Destructor invoked";
+	}
+};
+int main() 
+{
+	Employee e;
+	cout << "Size is:" << sizeof(e);
+	getchar();
+	return 0;
+}
