4109debcb8d66ab9e3664056b4604f89b7b8f51e
yuvaroshit <yuvaroshit@gmail.com>
2019-03-06 20:44:35 -0800

    strops

---
+++ ncrwork/C/Additional/StrOps/StrOps/Source.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<string.h>
+#define size 100
+//String Reverse Functions
+char* STRREV(char *str1)
+{
+	int i, j;//hold the loop running vars
+	for (i = 0, j = strlen(str1) - 1; i < j; i++, j--)
+	{
+		char ch;
+		ch = str1[i];
+		str1[i] = str1[j];
+		str1[j] = ch;
+	}
+	return str1;
+}
+//Compares 2 strings
+int STRCMP(char *str1, char *str2)
+{
+	while (*str1 == *str2)
+	{
+		if ((*str1 == '\0') || (*str2 == '\0'))
+			return 0;
+		str1++;
+		str2++;
+	}
+	return ((*str1) - (*str2));
+}
+//Concatenates the strings
+char* STRCAT(char *str1, char *str2)
+{
+	char *og = str1;
+	while (*str1 != '\0')
+	{
+		*str1++;
+	}
+	while ((*str1 = *str2) && (*str2 != '\0'))
+	{
+		*str1++;
+		*str2++;
+	}
+	return og;
+}
+//String Copy Function
+char* STRCPY(char *str1, char *str2)
+{
+	char *og;
+	og = str1;
+	while ((*str1 = *str2) != '\0')
+	{
+		*str1++;
+		*str2++;
+	}
+	return og;
+}
+int main()
+{
+	int choice;
+	char str1[size];
+	char str2[size];
+	printf("1.String Reverse\n2.String Compare\n3.String Concatenate\n4.String Copy\n");
+	scanf_s("%d", &choice);
+	//Evaluates User's Choice
+	switch (choice)
+	{
+	case 1: printf("Enter the string\n");
+		scanf_s("%s", str1, 10);
+		printf("%s", STRREV(str1));
+		break;
+	case 2:printf("Enter string 1\n");
+		scanf_s("%s", str1, 10);
+		printf("Enter string 2\n");
+		scanf_s("%s", str2, 10);
+		printf("%d", STRCMP(str1, str2));
+		break;
+	case 3:printf("Enter string 1\n");
+		scanf_s("%s", str1, 10);
+		printf("Enter string 2\n");
+		scanf_s("%s", str2, 10);
+		printf("%s", STRCAT(str1, str2));
+		break;
+	case 4:printf("Enter string 1\n");
+		scanf_s("%s", str1, 10);
+		printf("Enter string 2\n");
+		scanf_s("%s", str2, 10);
+		printf("%s", STRCPY(str1, str2));
+		break;
+	default:printf("Enter a valid option");
+		break;
+	}
+	system("pause");
+	return 0;
+}
